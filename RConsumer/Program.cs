8dfe91dde743fee1f61a976750652ffb27bc0dff
raphacarvlh <devone.r8@gmail.com>
2024-04-14 18:42:37 -0300

    portfólio filas

---
+++ RConsumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+
+
+
+namespace RConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var factory = new ConnectionFactory { HostName = "localhost" };
+             var connection = factory.CreateConnection();
+             var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "TestApp",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                try {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Received {message}");
+
+                    channel.BasicAck(ea.DeliveryTag, false);
+
+                }
+                catch (Exception ex)
+                {
+                    channel.BasicNack(ea.DeliveryTag, false, false);
+                }
+                
+            };
+            channel.BasicConsume(queue: "TestApp",
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RProducer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+
+
+namespace RProducer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            IConnection connection1 = factory.CreateConnection();
+             var connection = connection1;
+             var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "TestApp",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+
+            int count = 0;
+            while (true)
+            {
+                string message = $"{count++} Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: string.Empty,
+                                     routingKey: "TestApp",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($" [x] Sent {message}");
+                System.Threading.Thread.Sleep(200);
+
+               // Console.WriteLine(" Press [enter] to exit.");
+               // Console.ReadLine();
+            }
+        }
+    }
+}
